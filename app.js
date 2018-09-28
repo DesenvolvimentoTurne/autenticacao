e5fd205b9fffd179e6f106df2cab0d3b78983224
TozziPaulo <33462630+TozziPaulo@users.noreply.github.com>
2018-09-28 13:30:05 -0300

    teste
    teste

--- app.js
+++ app.js
@@ -1,50 +1,132 @@
-/**
-* Module dependencies.
-*/
-var express = require('express')
-  , routes = require('./routes')
-  , user = require('./routes/user')
-  , http = require('http')
-  , path = require('path');
-//var methodOverride = require('method-override');
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressValidator = require('express-validator');
+var multer = require('multer');
+var upload = multer({dest: './uploads'});
+var flash = require('connect-flash');
+var bcrypt = require('bcryptjs');
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+var db = mongoose.connection;
+
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
 var app = express();
-var mysql      = require('mysql');
-var bodyParser=require("body-parser");
-var connection = mysql.createConnection({
-              host     : 'localhost',
-              user     : 'root',
-              password : 'root',
-              database : 'testlogin'
-            });
- 
-connection.connect();
- 
-global.db = connection;
- 
-// all environments
-app.set('port', process.env.PORT || 8080);
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: false }));
+
+//console.log('log 1 dentro do app.js');
+
+// // view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+//console.log('Log 2 dentro do app.js');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//console.log('Log 3 dentro do app.js');
+
+// Handle Sessions
 app.use(session({
-              secret: 'keyboard cat',
-              resave: false,
-              saveUninitialized: true,
-              cookie: { maxAge: 60000 }
-            }))
- 
-// development only
- 
-app.get('/', routes.index);//call for main index page
-app.get('/signup', user.signup);//call for signup page
-app.post('/signup', user.signup);//call for signup post 
-app.get('/login', routes.index);//call for login page
-app.post('/login', user.login);//call for login post
-app.get('/home/dashboard', user.dashboard);//call for dashboard page after login
-app.get('/home/logout', user.logout);//call for logout
-app.get('/home/profile',user.profile);//to render users profile
-//Middleware
-app.listen(8080)
+   secret:'secret',
+  saveUninitialized: true,
+   resave: true
+ }));
+
+//console.log('Log 3 dentro do app.js');
+
+ // Passport
+ app.use(passport.initialize());
+ app.use(passport.session());
+
+ // Validator
+ app.use(expressValidator({
+   errorFormatter: function(param, msg, value) {
+       var namespace = param.split('.')
+       , root    = namespace.shift()
+       , formParam = root;
+
+     while(namespace.length) {
+       formParam += '[' + namespace.shift() + ']';
+     }
+     return {
+       param : formParam,
+       msg   : msg,
+       value : value
+     };
+   }
+ }));
+
+//console.log('Log 4 dentro do app.js');
+
+ app.use(cookieParser());
+ app.use(express.static(path.join(__dirname, 'public')));
+
+ app.use(flash());
+ app.use(function (req, res, next) {
+   res.locals.messages = require('express-messages')(req, res);
+   next();
+ });
+
+//console.log('Log 5 dentro do app.js');
+
+ app.get('*', function(req, res, next){
+  res.locals.user = req.user || null;
+   next();
+ });
+
+//console.log('Log 6 dentro do app.js');
+
+ app.use('/', routes);
+ app.use('/users', users);
+
+//console.log('Log 7');
+
+// catch 404 and forward to error handler
+ app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+ });
+
+ // error handlers
+
+//console.log('Log 8 dentro do app.js');
+
+// development error handler
+ // will print stacktrace
+ if (app.get('env') === 'development') {
+   app.use(function(err, req, res, next) {
+     res.status(err.status || 500);
+     res.render('error', {
+       message: err.message,
+       error: err
+     });
+   });
+ }
+
+//console.log('Log 9 dentro do app.js');
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+   res.render('error', {
+     message: err.message,
+     error: {}
+   });
+ });
+
+
+ module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
-/*
-* GET home page.
-*/
- 
-exports.index = function(req, res){
-    var message = '';
-  res.render('index',{message: message});
- 
-};
+var express = require('express');
+var router = express.Router();
+
+//console.log('Log 1 dentro do index.js');
+
+/* GET home page. */
+router.get('/', ensureAuthenticated, function(req, res, next) {
+  //console.log('Dentro do GET do index.js');
+  res.render('index', { title: 'Members' });
+});
+
+//console.log('Log 2 dentro do index.js');
+
+function ensureAuthenticated(req, res, next){
+	//console.log('Dentro da function ensureAuthenticated');
+	if(req.isAuthenticated()){
+		return next();
+	}
+	res.redirect('/users/login');
+}
+
+module.exports = router;
